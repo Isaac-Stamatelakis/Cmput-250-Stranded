ea7f9269f89927691dd878781654ba38a2be8a3e
Katrina Alejo <kalejo@ualberta.ca>
2024-11-08 15:11:42 -0700

    Updated the cutscene

--- Assets/Scripts/Dialogue/UI/DialogUIController.cs
+++ Assets/Scripts/Dialogue/UI/DialogUIController.cs
@@ -2,20 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Dialogue {
+namespace Dialogue
+{
     public class DialogUIController : MonoBehaviour
     {
         [SerializeField] private DialogueBoxUI dialogueBoxUI;
         private static DialogUIController instance;
         public static DialogUIController Instance => instance;
         public bool ShowingDialog => dialogueBoxUI.gameObject.activeInHierarchy;
-        public void Awake() {
+
+        // Add fields for the last dialogue name and the ending scene name
+        [SerializeField] private string lastDialogName = "nd_p3"; 
+        [SerializeField] private string endingSceneName = "ending cutscene"; 
+
+        public void Awake()
+        {
             instance = this;
         }
-        public void DisplayDialogue(DialogObject dialogObject) {
+
+        public void DisplayDialogue(DialogObject dialogObject)
+        {
             dialogueBoxUI.gameObject.SetActive(true);
             dialogueBoxUI.DisplayDialogue(dialogObject);
+
+            // Check if the current dialogue is the last one
+            if (dialogObject != null && dialogObject.name == lastDialogName)
+            {
+                PlayEndingCutscene();
+            }
+        }
+
+        private void PlayEndingCutscene()
+        {
+            Debug.Log("Playing ending cutscene...");
+            NextSceneLoader.Instance.LoadScene(endingSceneName);
         }
     }
 }
-
