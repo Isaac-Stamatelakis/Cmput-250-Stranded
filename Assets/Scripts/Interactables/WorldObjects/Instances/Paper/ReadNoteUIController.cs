ccb93487a85b3faea9a50596136012c31788cd9c
Isaac-Stamatelakis <97195929+Isaac-Stamatelakis@users.noreply.github.com>
2024-12-06 08:14:22 -0700

    Added interactable paper UI objects

---
+++ Assets/Scripts/Interactables/WorldObjects/Instances/Paper/ReadNoteUIController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using PlayerModule;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class ReadNoteUIController : MonoBehaviour
+{
+    private static ReadNoteUIController instance;
+    public static ReadNoteUIController Instance => instance;
+    [SerializeField] private TextMeshProUGUI header;
+    [SerializeField] private TextMeshProUGUI content;
+    public void Awake()
+    {
+        instance = this;
+        gameObject.SetActive(false);
+    }
+
+    public void Start()
+    {
+        
+    }
+
+    public void Display(string header, string content)
+    {
+        Player.Instance.setDialog(true);
+        gameObject.SetActive(true);
+        this.header.text = header;
+        this.content.text = content;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameObject.SetActive(false);
+            Player.Instance.setDialog(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Interactables/WorldObjects/Instances/Paper/ReadableNote.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Rooms;
+using UnityEngine;
+
+public class ReadableNote : InteractableGameObject
+{
+    [SerializeField, TextArea(10, 10)] public string Header;
+    [SerializeField, TextArea(10, 10)] public string Content;
+    public override void interact()
+    {
+        bool clear = LevelManager.getInstance().CurrentLevel.CurrentRoomClear();
+        if (!clear)
+        {
+            return;
+        }
+        ReadNoteUIController.Instance.Display(Header,Content);
+    }
+
+    public override string getInteractText()
+    {
+        bool clear = LevelManager.getInstance().CurrentLevel.CurrentRoomClear();
+        return !clear ? "<color=red>Not a Safe Reading Environment</color>" : $"<color=green>Read Note</color>";
+    }
+}
